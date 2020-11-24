a321ea4a6657fc09ed23aa223a47284b0bf16725
Manuel Takata <54631891+Takata21@users.noreply.github.com>
2020-11-24 16:04:31 -0500

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,7 @@
 module.exports = {
     database: {
-        URI: 'mongodb://localhost/swclassdb'
+        URI: 'mongodb://localhost/swclassdb',
+        URI:mongodb+srv://root:211011@cluster0.rrqje.mongodb.net/SWCLASS?retryWrites=true&w=majority
     }
 
 }
