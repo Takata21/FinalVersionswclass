2fed1f2ecfd961ee627440b15b925d438873b121
Manuel Takata <takata211011@gmail.com>
2020-11-24 15:40:04 -0500

    before to share with heroku

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -58,7 +58,7 @@ userCtrl.SignUp = async(req, res) => {
 
 userCtrl.login = passport.authenticate('local', {
     failureRedirect: '/user/login',
-    successRedirect: '/user/home',
+    successRedirect: '/class/home',
     failureFlash: true
 })
 
--- src/routes/class.routes.js
+++ src/routes/class.routes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express')
 const router = Router()
-const { renderHome, renderClass, createClass } = require('../controllers/class.crontollers')
+const { renderHome, renderClass, createClass, cancelClass } = require('../controllers/class.crontollers')
 router.get('/class/home', renderHome)
 router.post('/class/newclass', createClass)
 router.get('/class/myclass', renderClass)
+router.post('/class/myclass/delete/:class_id', cancelClass)
+
 
 
 module.exports = router
